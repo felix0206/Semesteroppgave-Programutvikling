31401af200dae467040f7a980627c2e263022602
jørgen <s331352@oslomet.no>
2020-04-03 11:48:52 +0200

    admin side og controller

--- src/src/Fxml/AdminSide.java
+++
@@ -1,4 +0,0 @@
-package Fxml;
-
-public class AdminSide {
-}
---
+++ src/src/Fxml/AdminSideController.java
@@ -0,0 +1,9 @@
+package Fxml;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class AdminSideController {
+    @FXML
+    public TableColumn Navn, Email, TypeBil, Hestekrefter, Interior, Farge, Felger, Pris;
+}
