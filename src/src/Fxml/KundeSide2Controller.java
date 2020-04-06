0cae9c6fb2b7d2fb21b1d6daa23f8cc2f2ec5630
Abdi <abdi.hs@live.no>
2020-04-06 13:34:47 +0200

    Ny kundeside og kontroller

--- src/src/Fxml/KundeSide2Controller.java
+++ src/src/Fxml/KundeSide2Controller.java
@@ -1,4 +1,15 @@
 package Fxml;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class KundeSide2Controller {
 }
