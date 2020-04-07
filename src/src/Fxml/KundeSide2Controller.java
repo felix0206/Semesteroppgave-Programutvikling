5e7cd047ffe10e3cc8fd3beef65be3513327f16e
Abdi <abdi.hs@live.no>
2020-04-07 22:36:41 +0200

    Ny kundeside og kontroller

--- src/src/Fxml/KundeSide2Controller.java
+++ src/src/Fxml/KundeSide2Controller.java
@@ -1,4 +1,7 @@
 package Fxml;
+import com.sun.javafx.scene.control.skin.ChoiceBoxSkin;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,9 +10,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.util.Observable;
 
 public class KundeSide2Controller {
+
+    ObservableList Hestekrefterlist= FXCollections.observableArrayList("150 hk", "200 hk" , "300 hk" );
+
+    @FXML
+    private ChoiceBox Hestekrefterbox;
+
+    @FXML
+    private void initialize(){
+
+        Hestekrefterbox.setValue("Velg Hestekrefter");
+
+        Hestekrefterbox.setItems(Hestekrefterlist);
+
+
+    }
+
 }
--- src/src/Fxml/Main.java
+++ src/src/Fxml/Main.java
@@ -19,6 +19,6 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.show();
 
-    }
 
+    }
 }
