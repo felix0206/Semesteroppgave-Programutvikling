e6aa04390bbc949e1170fd2c51362cbc60c10b58
Abdi <abdi.hs@live.no>
2020-04-09 20:13:13 +0200

    Kundeside2 checkboxes

--- src/src/Fxml/KundeSide2Controller.java
+++ src/src/Fxml/KundeSide2Controller.java
@@ -20,19 +20,50 @@ import java.util.Observable;
 
 public class KundeSide2Controller {
 
-    ObservableList Hestekrefterlist= FXCollections.observableArrayList("150 hk", "200 hk" , "300 hk" );
+    ObservableList Hestekrefterlist= FXCollections.observableArrayList("Velg Hestekrefter","150 hk", "200 hk" , "300 hk" );
+    ObservableList Fargerlist= FXCollections.observableArrayList("Velg Farge","Svart", "Hvit" , "Rød", "Bronse" );
+    ObservableList Interiørlist= FXCollections.observableArrayList("Velg Interiør","Standar", "Sport" , "Supreme" );
+    ObservableList Felgerlist= FXCollections.observableArrayList("Velg Felger","18 tommer", "20 tommer" , "24 ");
 
     @FXML
     private ChoiceBox Hestekrefterbox;
+    @FXML
+    private ChoiceBox Fargebox;
+    @FXML
+    private ChoiceBox Interiørbox;
+    @FXML
+    private ChoiceBox Felgerbox;
+
 
     @FXML
     private void initialize(){
 
         Hestekrefterbox.setValue("Velg Hestekrefter");
-
         Hestekrefterbox.setItems(Hestekrefterlist);
+
+        Fargebox.setValue("Velg Farge");
+        Fargebox.setItems(Fargerlist);
+
+        Interiørbox.setValue("Velg Interiør");
+        Interiørbox.setItems(Interiørlist);
+
+        Felgerbox.setValue("Velg Felger");
+        Felgerbox.setItems(Felgerlist);
 
 
     }
 
+
+    public void setFargebox(ChoiceBox fargebox) {
+        Fargebox = fargebox;
+    }
+
+    public void setInteriørbox(ChoiceBox interiørbox) {
+        Interiørbox = interiørbox;
+    }
+
+    public void setFelgerbox(ChoiceBox felgerbox) {
+        Felgerbox = felgerbox;
+    }
 }
+
