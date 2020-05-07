c3d0bc66dba57c419ec43f334a67007277afa1d2
felix <felixfotball99@gmail.com>
2020-05-07 23:56:51 +0200

    --> lagt til confirmation hvis kunde velger å lage egen bil.

--- src/src/Fxml/KundeSide2Controller.java
+++ src/src/Fxml/KundeSide2Controller.java
@@ -9,9 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -19,6 +17,7 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Optional;
 
 public class KundeSide2Controller {
     Exceptions exceptions = new Exceptions("");
@@ -124,6 +123,21 @@ public class KundeSide2Controller {
     //knapp for å gå til oppsummeringssidenm hvor bilen og dens komponenter blir listet opp.
     public void Oppsummering(ActionEvent event) throws IOException {
 
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("FULLFØR KJØP");
+        alert.setHeaderText(null);
+        alert.setContentText("Fullfør kjøpet? \nklikk ok for fullfør \nklikk cancel for å avbryte");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+        //Oppretter en filewriter for å skrive til csv-filen
+        //sånn at admin vil få opp den nye bilen med navn og epost i tableview neste gang den åpnes.
+        FileWriter fileWriter = new FileWriter("src/src/save_load/kundeDataBase.csv", true);
+
+        fileWriter.write(navn + ";" + epost + ";" + typebil + ";" + hester + ";" + interior + ";" + farge +
+                ";" + felger + ";" + getPris()+"\n");
+        fileWriter.close();
+        //lukker filewriteren.
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("KundeSisteSide.fxml"));
         Parent root = loader.load();
 
@@ -163,7 +177,10 @@ public class KundeSide2Controller {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
 
         window.setScene(scene);
-        window.show();
+        window.show();}
+        else{
+            return;
+        }
 
     }
 
@@ -192,14 +209,8 @@ public class KundeSide2Controller {
             LagreValg.setVisible(false);
         }
 
-        //Oppretter en filewriter for å skrive til csv-filen
-        //sånn at admin vil få opp den nye bilen med navn og epost i tableview neste gang den åpnes.
-        FileWriter fileWriter = new FileWriter("src/src/save_load/kundeDataBase.csv", true);
 
-        fileWriter.write(navn + ";" + epost + ";" + typebil + ";" + hester + ";" + interior + ";" + farge +
-                        ";" + felger + ";" + getPris()+"\n");
-        fileWriter.close();
-        //lukker filewriteren.
+
 
 
         //TODO: ta bort denne før vi leverer.
